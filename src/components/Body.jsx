178eb45e4ea86b391106a91ebdd8ed7dca8f5c8b
ankitxcoding <ankitxcoding@gmail.com>
2024-02-25 17:26:10 +0530

    made body component for the website

---
+++ src/components/Body.jsx
@@ -0,0 +1,11 @@
+import RestaurantContainer from "./RestaurantContainer";
+
+const Body = () => {
+
+  return (
+    <div className="m-4">
+      <RestaurantContainer />
+    </div>
+  );
+};
+export default Body;
