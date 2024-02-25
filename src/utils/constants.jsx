b3f4acf11d976a02e3d69d7867c2911fd529fc87
ankitxcoding <ankitxcoding@gmail.com>
2024-02-25 17:29:43 +0530

    made constants and exported some constants for the website

---
+++ src/utils/constants.jsx
@@ -0,0 +1,7 @@
+export const LOGO =
+  "https://play-lh.googleusercontent.com/rolUGAfG9YvT5ytKbIHAR9GThBa_nR-lU33UbFzqJinj1baXcQDrCc0vZmGQxw_rl2Q=w240-h480-rw";
+
+export const RES_CARD_API =
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627";
+
+export const RES_IMG="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660,h_500,c_fill/"
