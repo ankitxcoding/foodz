ddf3a19dff412a3416c2350d88d10141952102e9
ankitxcoding <ankitxcoding@gmail.com>
2024-02-26 12:11:33 +0530

    added search functionality and show top rated restaurants button

--- src/components/RestaurantContainer.jsx
+++ src/components/RestaurantContainer.jsx
@@ -4,6 +4,13 @@ import RestaurantCard from "./RestaurantCard";
 
 const RestaurantContainer = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+
+  const [searchRestaurant, setSearchRestaurant] = useState("");
+
+  const [topRatedRestaurantsText, setTopRatedRestaurantsText] = useState(
+    "Top Rated Restaurants"
+  );
 
   const fetchData = async () => {
     const data = await fetch(RES_CARD_API);
@@ -20,6 +27,7 @@ const RestaurantContainer = () => {
     };
     const restaurantData = await checkJsonData(json);
     setListOfRestaurants(restaurantData);
+    setFilteredRestaurants(restaurantData);
     console.log(json);
   };
 
@@ -27,14 +35,65 @@ const RestaurantContainer = () => {
     fetchData();
   }, []);
 
+  const topRatedRestaurants = () => {
+    const filteredTopRestaurants = listOfRestaurants.filter(
+      (restaurant) => restaurant?.info?.avgRating >= 4.5
+    );
+    filteredRestaurants.length === listOfRestaurants.length
+      ? setFilteredRestaurants(filteredTopRestaurants)
+      : setFilteredRestaurants(listOfRestaurants);
+    topRatedRestaurantsText === "Top Rated Restaurants"
+      ? setTopRatedRestaurantsText("See All Restaurants")
+      : setTopRatedRestaurantsText("Top Rated Restaurants");
+  };
+
+  const searchForRestaurants = () => {
+    const searchResult = listOfRestaurants.filter((restaurant) =>
+      restaurant?.info?.name
+        .toLowerCase()
+        .includes(searchRestaurant.toLowerCase())
+    );
+    setFilteredRestaurants(searchResult);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      searchForRestaurants();
+    }
+  };
+
   return (
-    <div>
-      <ul className="flex flex-wrap">
-        {listOfRestaurants?.map((restaurant) => (
-          <RestaurantCard key={restaurant.info.id} resDetails={restaurant} />
-        ))}
-      </ul>
-    </div>
+    <>
+      <div className="m-4">
+        <input
+          className="border-2 border-black px-2 m-1 rounded-md"
+          type="text"
+          placeholder="Search Restaurants"
+          value={searchRestaurant}
+          onChange={(e) => setSearchRestaurant(e.target.value)}
+          onKeyDown={handleKeyDown}
+        />
+        <button
+          className="border-2 border-black px-2 m-1 rounded-md"
+          onClick={searchForRestaurants}
+        >
+          Search
+        </button>
+        <button
+          className="border-2 border-black px-2 m-1 rounded-md"
+          onClick={topRatedRestaurants}
+        >
+          {topRatedRestaurantsText}
+        </button>
+      </div>
+      <div>
+        <ul className="flex flex-wrap">
+          {filteredRestaurants.map((restaurant) => (
+            <RestaurantCard key={restaurant?.info?.id} resDetails={restaurant} />
+          ))}
+        </ul>
+      </div>
+    </>
   );
 };
 export default RestaurantContainer;
