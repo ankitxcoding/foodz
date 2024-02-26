b66f8684dcee0519316855b3a78e27c06cc03402
ankitxcoding <ankitxcoding@gmail.com>
2024-02-26 19:13:29 +0530

    made restaurant cards looks good by changing bg color and adding shodow and same for shimmer ui

--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -17,7 +17,7 @@ const RestaurantCard = (prop) => {
       <img src={RES_IMG + cloudinaryImageId} alt="restaurantImages" />
       <h1 className="p-2 font-bold">{name}</h1>
       <h2 className="p-2 font-semibold">{cuisines.join(", ")}</h2>
-      <h3 className="p-2 font-semibold"><i className="fa-solid fa-star"></i>{avgRating}</h3>
+      <h3 className="p-2 font-semibold">{avgRating}</h3>
       <h3 className="p-2 font-semibold">{costForTwo}</h3>
       <h3 className="p-2 font-semibold">{deliveryTime + " minutes"}</h3>
     </div>
