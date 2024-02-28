397c69707c3cb114bb1d2f233d136ab26ba5c915
ankitxcoding <ankitxcoding@gmail.com>
2024-02-28 14:28:20 +0530

    made a custom hook for the restaurant information

---
+++ src/hooks/useRestaurantInfo.jsx
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+import { RES_MENU_API } from "../utils/constants";
+
+const useRestaurantInfo = (resId) => {
+  const [restaurantInfo, setRestaurantInfo] = useState(null);
+  useEffect(() => {
+    fetchMenu();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const fetchMenu = async () => {
+    const data = await fetch(RES_MENU_API + resId);
+    const json = await data.json();
+    setRestaurantInfo(json?.data?.cards[2]?.card?.card?.info);
+  };
+  return restaurantInfo;
+};
+
+export default useRestaurantInfo;
