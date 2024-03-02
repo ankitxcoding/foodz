401b5336e498abebbf0f6aafbdc11e91169e5b77
ankitxcoding <ankitxcoding@gmail.com>
2024-03-02 14:37:06 +0530

    added add+ button on menu items images

--- src/components/ItemsList.jsx
+++ src/components/ItemsList.jsx
@@ -13,18 +13,23 @@ const ItemList = (prop) => {
           <div className="flex justify-between w-full">
             <div className="w-full">
               <h3 className="font-semibold text-gray-700">
-                {item?.card?.info?.name} - ₹{item?.card?.info?.defaultPrice/100 || item?.card?.info?.price / 100}
+                {item?.card?.info?.name} - ₹
+                {item?.card?.info?.defaultPrice / 100 ||
+                  item?.card?.info?.price / 100}
               </h3>
               <p className="text-xs w-4/5 text-gray-500">
                 {item?.card?.info?.description}
               </p>
             </div>
-            <div className="w-40">
+            <div className="w-40 relative">
               <img
                 src={RES_IMG + item?.card?.info?.imageId}
                 alt="itemImg"
                 className="w-full rounded-md"
               />
+              <button className="mx-9 py-2 px-4 text-xs font-semibold bg-black text-lime-500 border border-gray-500 rounded-md absolute -bottom-1 hover:text-white">
+                ADD⁺
+              </button>
             </div>
           </div>
         </div>
